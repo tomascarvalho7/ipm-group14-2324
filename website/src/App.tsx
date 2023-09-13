35d2dc81319ba86316bfe2a22218c1eb8df07db6
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2023-09-13 10:49:19 +0100

    started site development

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,26 +6,25 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="title">
+        <h1>Project Title</h1>
+        <h2>Human-Computer Interaction p.2</h2>
       </div>
-      <h1>Vite + React</h1>
+      <div id="authors">
+        <h3>(photo) 67208 - Francisco Barreiras</h3>
+        <h3>(photo) 67209 - Tomás Carvalho</h3>
+        <h3>(photo) 67210 - Miguel Palma</h3>
+        <h3>(photo) 67211 - Alexandre Madeira</h3>
+      </div>
+      <div id="project" className="card">
+        <button>Project</button>
+      </div>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
