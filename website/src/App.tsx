20fb083882f36845fb9f5de6bc66772465f34d33
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2023-09-17 19:29:46 +0100

    added authors to website

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,20 +1,39 @@
 import { useState } from 'react'
 import './App.css'
 
+function AuthorsSection() {
+
+  const authors = [
+    { name: 'Francisco Barreiras', number: 67208, photo: 'src/assets/francisco.jpg' },
+    { name: 'Tomás Carvalho', number: 67209, photo: 'src/assets/tomas.jpg' },
+    { name: 'Miguel Palma', number: 67210, photo: 'src/assets/miguel.jpg' },
+    { name: 'Alexandre Madeira', number: 67211, photo: 'src/assets/alexandre.jpg' },
+  ]
+
+  return (
+    <div className="authors-section">
+      <div className="authors-container">
+        {authors.map((author) => (
+          <div className="author-card" key={author.number}>
+            <img src={author.photo} alt={author.name} className="author-photo" />
+            <p><b>{author.name}</b></p>
+            <p>{author.number}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <AuthorsSection />
       <div id="title">
         <h1>Project Title</h1>
         <h2>Human-Computer Interaction p.2</h2>
-      </div>
-      <div id="authors">
-        <h3>(photo) 67208 - Francisco Barreiras</h3>
-        <h3>(photo) 67209 - Tomás Carvalho</h3>
-        <h3>(photo) 67210 - Miguel Palma</h3>
-        <h3>(photo) 67211 - Alexandre Madeira</h3>
       </div>
       <div id="project" className="card">
         <button>Project</button>
