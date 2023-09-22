89b15e0391c2a89c6dd76acce1c9f1aacc7ec01d
miguelpalma125 <miguel.palma.125@gmail.com>
2023-09-22 16:05:14 +0100

    Navigation arrows working

--- website/src/routes/Author.tsx
+++ website/src/routes/Author.tsx
@@ -1,29 +1,46 @@
 import { Navigate, useParams } from "react-router-dom";
-import { getAuthor } from "../authors";
+import { authors, getAuthor } from "../authors";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 export function Author() {
   const params = useParams()
   const number = params["number"] ?? ""
   const author = getAuthor(parseInt(number))
+
   if (!author) return <Navigate to="/" />
+
+  const handleLeftArrowClick = () => {
+    let newNumber = parseInt(number) - 1;
+    let newAuthorNumber = authors.find(author => author.number == newNumber)?.number ?? authors[authors.length - 1].number;
+
+    window.location.href = `/authors/${newAuthorNumber}`;
+  };
+
+  const handleRightArrowClick = () => {
+    let newNumber = parseInt(number) + 1;
+    let newAuthorNumber = authors.find(author => author.number == newNumber)?.number ?? authors[0].number;
+
+    window.location.href = `/authors/${newAuthorNumber}`;
+  };
 
   return (
     <div className="authorinfo">
-      <div className="nav-arrow left-arrow">
-        <span>&#8592;</span>
+      <div className="nav-arrow left-arrow" onClick={handleLeftArrowClick}>
+        <span><i className="bi bi-arrow-left-short"></i></span>
       </div>
 
       <h1>{author?.name}</h1>
-      
+  
       <div className="authorbox">
         <h3>Individual tasks:</h3>
         <a href={`/assets/${author?.number}/assignment1.pdf`} target="_blank">Assignment 1</a>
       </div>
       
-      <div className="nav-arrow right-arrow">
-        <span>&#8594;</span>
+      <div className="nav-arrow right-arrow" onClick={handleRightArrowClick}>
+        <span><i className="bi bi-arrow-right-short"></i></span>
       </div>
-
     </div>
   )
 }
