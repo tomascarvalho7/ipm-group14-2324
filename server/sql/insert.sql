a02d093a2e45e7fbdc492a0b2cf9db7ecfc7ba43
miguelpalma125 <miguel.palma.125@gmail.com>
2023-10-06 19:27:20 +0100

    added insert

--- server/sql/insert.sql
+++ server/sql/insert.sql
@@ -1,3 +1,36 @@
 begin transaction;
 
+insert into syncshop.user (email, name, password, token) values
+    ('user1@gmail.com', 'user1', 'user1pwd', 'user1token');
+
+insert into syncshop.user (email, name, password, token) values 
+    ('user2@gmail.com', 'user2', 'user2pwd', 'user2token');
+
+insert into synchshop.list (name, code) values 
+    ('list1', 'list1code');
+
+insert into synchshop.list (name, code) values 
+    ('list2', 'list2code');
+
+insert into synshop.product (list_id, name, date, bought, categories, priority) values
+    (1, 'product1', '2023-01-01 00:00:00', false, 'category1', 1);
+
+insert into synshop.product (list_id, name, date, bought, categories, priority) values
+    (2, 'product2', '2023-01-01 00:00:00', false, 'category1', 1);
+
+insert into synshop.product (list_id, name, date, bought, categories, priority) values
+    (1, 'product3', '2023-01-01 00:00:00', false, 'category2', 2);
+
+insert into synshop.list_user (list_id, user_id) values
+    (1, 1);
+
+insert into synshop.list_user (list_id, user_id) values
+    (1, 2);
+
+insert into synshop.list_user (list_id, user_id) values
+    (2, 1);
+
+insert into synshop.list_user (list_id, user_id) values
+    (2, 2);
+
 commit transaction;
