cf895101f86d0317ddfac8a2081dbd206b89d78c
Tomás Carvalho <tomascarvalho7@gmail.com>
2023-10-07 03:04:49 +0100

    Fixed insert script

--- server/sql/insert.sql
+++ server/sql/insert.sql
@@ -6,31 +6,31 @@ insert into syncshop.user (email, name, password, token) values
 insert into syncshop.user (email, name, password, token) values 
     ('user2@gmail.com', 'user2', 'user2pwd', 'user2token');
 
-insert into synchshop.list (name, code) values 
+insert into syncshop.list (name, code) values 
     ('list1', 'list1code');
 
-insert into synchshop.list (name, code) values 
+insert into syncshop.list (name, code) values 
     ('list2', 'list2code');
 
-insert into synshop.product (list_id, name, date, bought, categories, priority) values
+insert into syncshop.product (list_id, name, date, bought, categories, priority) values
     (1, 'product1', '2023-01-01 00:00:00', false, 'category1', 1);
 
-insert into synshop.product (list_id, name, date, bought, categories, priority) values
+insert into syncshop.product (list_id, name, date, bought, categories, priority) values
     (2, 'product2', '2023-01-01 00:00:00', false, 'category1', 1);
 
-insert into synshop.product (list_id, name, date, bought, categories, priority) values
+insert into syncshop.product (list_id, name, date, bought, categories, priority) values
     (1, 'product3', '2023-01-01 00:00:00', false, 'category2', 2);
 
-insert into synshop.list_user (list_id, user_id) values
+insert into syncshop.list_user (list_id, user_id) values
     (1, 1);
 
-insert into synshop.list_user (list_id, user_id) values
+insert into syncshop.list_user (list_id, user_id) values
     (1, 2);
 
-insert into synshop.list_user (list_id, user_id) values
+insert into syncshop.list_user (list_id, user_id) values
     (2, 1);
 
-insert into synshop.list_user (list_id, user_id) values
+insert into syncshop.list_user (list_id, user_id) values
     (2, 2);
 
 commit transaction;
