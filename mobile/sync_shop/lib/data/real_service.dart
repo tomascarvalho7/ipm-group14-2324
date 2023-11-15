86d2b4f83538605cb535764f1a421857fabc78a7
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2023-11-15 11:54:31 +0000

    implemented new product service

--- mobile/sync_shop/lib/data/real_service.dart
+++ mobile/sync_shop/lib/data/real_service.dart
@@ -2,11 +2,28 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:sync_shop/config.dart';
 
 class RealService {
-  ///´Initialize service environment
+  // Initialize service environment
   void load() async {
     await Supabase.initialize(
       url: supabaseUrl,
       anonKey: supabaseKey,
     );
+  }
+
+  Future<void> insertProduct(
+    int listId,
+    String name,
+    List<String> categories,
+    int priority,
+  ) async {
+    final SupabaseClient client = Supabase.instance.client;
+
+    await client.from('product').insert({
+      "list_id": 1,
+      "name": name,
+      "bought": false,
+      "categories": categories,
+      "priority": priority,
+    });
   }
 }
