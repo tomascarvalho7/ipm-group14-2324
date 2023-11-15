60050fb7c4045db362e9bee7a4dfd5100c6d3361
alexmadeira23 <alexandrealemaomadeira@gmail.com>
2023-11-15 22:10:22 +0000

    integrated service in add product

--- mobile/sync_shop/lib/presentation/new_product/new_product_screen.dart
+++ mobile/sync_shop/lib/presentation/new_product/new_product_screen.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sync_shop/data/real_service.dart';
 import 'package:sync_shop/presentation/new_product/priority_selector.dart';
 import 'package:sync_shop/presentation/utils/rectangular_button.dart';
 import 'package:sync_shop/presentation/utils/text_input.dart';
 import 'package:sync_shop/screen_template.dart';
 
 class NewProcuctScreen extends StatefulWidget {
-  const NewProcuctScreen({super.key});
+  const NewProcuctScreen({super.key, required this.listId});
+
+  final int listId;
 
   @override
   State<NewProcuctScreen> createState() => _NewProcuctScreenState();
 }
 
 class _NewProcuctScreenState extends State<NewProcuctScreen> {
+  bool _isLoading = false;
+
+  void setIsLoading(bool value) {
+    setState(() {
+      _isLoading = value;
+    });
+  }
+
   String _productName = '';
 
   void setProductName(String value) {
@@ -39,6 +51,7 @@ class _NewProcuctScreenState extends State<NewProcuctScreen> {
 
   @override
   Widget build(BuildContext context) {
+    RealService services = Provider.of<RealService>(context, listen: false);
     return buildScreenTemplateWidget(
         context,
         'New Product',
@@ -65,7 +78,10 @@ class _NewProcuctScreenState extends State<NewProcuctScreen> {
                     margin: const EdgeInsets.symmetric(horizontal: 20),
                     child: RectangularButton(
                       text: 'Categories',
-                      onPressed: () => debugPrint('Categories'),
+                      onPressed: () {
+                        debugPrint('Categories');
+                        setCategories(['Fruit']);
+                      },
                     ),
                   ),
                   const SizedBox(height: 20),
@@ -81,7 +97,24 @@ class _NewProcuctScreenState extends State<NewProcuctScreen> {
                 margin: const EdgeInsets.symmetric(horizontal: 20),
                 child: RectangularButton(
                   text: 'Add Product',
-                  onPressed: () => debugPrint('Add'),
+                  onPressed: _productName.isNotEmpty &&
+                          _categories.isNotEmpty &&
+                          !_isLoading
+                      ? () {
+                          setIsLoading(true);
+                          services
+                              .insertProduct(
+                            widget.listId,
+                            _productName,
+                            _categories,
+                            _priority,
+                          )
+                              .then((value) {
+                            setIsLoading(false);
+                            Navigator.pop(context);
+                          });
+                        }
+                      : null,
                 ),
               ),
             ],
--- mobile/sync_shop/lib/presentation/utils/rectangular_button.dart
+++ mobile/sync_shop/lib/presentation/utils/rectangular_button.dart
@@ -2,23 +2,45 @@ import 'package:flutter/material.dart';
 
 class RectangularButton extends StatelessWidget {
   final String text;
-  final VoidCallback onPressed;
+  final VoidCallback? onPressed;
 
-  const RectangularButton(
-      {super.key, required this.text, required this.onPressed});
+  const RectangularButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        shape: RoundedRectangleBorder(
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Material(
+      borderRadius: BorderRadius.circular(10),
+      color: Colors.transparent,
+      child: Ink(
+        decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(10),
+          color: onPressed != null
+              ? colorScheme.surface
+              : colorScheme.surface.withOpacity(0.5),
         ),
-      ),
-      child: Text(
-        text,
-        style: const TextStyle(fontSize: 16, color: Colors.black),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(10),
+          onTap: onPressed,
+          splashColor: Colors.grey.withOpacity(0.5),
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Center(
+              child: Text(
+                text,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: onPressed != null
+                        ? colorScheme.background
+                        : colorScheme.background.withOpacity(0.5)),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
